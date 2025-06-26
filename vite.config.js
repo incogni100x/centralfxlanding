b164f8bbbb0b72cd6d61138ef6f208a93e8823b6
incogni100x <duche20001@gmail.com>
2025-06-26 05:41:22 +0100

    feat: initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        terms: resolve(__dirname, 'terms.html'),
+      },
+    },
+  },
+})
